58425ed9f0127ff47ccfbf0bc508a64487d314b8
nikita <morphling322323@gmail.com>
2025-11-26 22:38:45 +0300

    Initial commit

---
+++ field-kinds-derive/target/debug/deps/derive-570e448813b5e021.d
@@ -0,0 +1,5 @@
+/home/nikita/RustroverProjects/field-kinds/field-kinds-derive/target/debug/deps/derive-570e448813b5e021.d: src/lib.rs
+
+/home/nikita/RustroverProjects/field-kinds/field-kinds-derive/target/debug/deps/libderive-570e448813b5e021.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ field-kinds-derive/target/debug/deps/derive-6b0e6e933d8475b7.d
@@ -0,0 +1,5 @@
+/home/nikita/RustroverProjects/field-kinds/field-kinds-derive/target/debug/deps/derive-6b0e6e933d8475b7.d: src/lib.rs
+
+/home/nikita/RustroverProjects/field-kinds/field-kinds-derive/target/debug/deps/libderive-6b0e6e933d8475b7.rmeta: src/lib.rs
+
+src/lib.rs:
