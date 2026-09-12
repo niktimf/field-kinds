b38ed7c292bcf0dc791fabc0cc351e1f80af26cb
nikita <niktimf@gmail.com>
2026-09-12 20:52:57 +0300

    chore: remove unused files

--- field-kinds-derive/target/debug/deps/derive-570e448813b5e021.d
+++
@@ -1,5 +0,0 @@
-/home/nikita/RustroverProjects/field-kinds/field-kinds-derive/target/debug/deps/derive-570e448813b5e021.d: src/lib.rs
-
-/home/nikita/RustroverProjects/field-kinds/field-kinds-derive/target/debug/deps/libderive-570e448813b5e021.rmeta: src/lib.rs
-
-src/lib.rs:
